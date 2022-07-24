68ddf13d1bc27462d67611b5f3adb8c4e20fcd6d
skullandbonesdao <skullandbonesdao@gmail.com>
2022-07-24 18:15:10 +0200

    fixing url redirect gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
