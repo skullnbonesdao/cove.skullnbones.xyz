f337fdb9d70aa1c36495edb599850b7498487f82
skullandbonesdao <skullandbonesdao@gmail.com>
2022-08-02 01:37:26 +0200

    fixed percentage color issue

--- src/components/special/PercentageElement.vue
+++ src/components/special/PercentageElement.vue
@@ -3,7 +3,11 @@
     :class="
       is_price === 0.0 || no_market
         ? 'text-yellow-500'
-        : calculatesPercentage > 0
+        : calculatesPercentageVWAPvsMarket(
+            price_vwap,
+            is_price,
+            no_market
+          ).toFixed(1) > 0
         ? 'text-green-500'
         : 'text-red-500'
     "
@@ -30,18 +34,5 @@ const props = defineProps({
   is_price: { type: Number, default: 0 },
   no_market: { type: Boolean, default: false },
 });
-
-function calculatesPercentage(): number {
-  if (props.is_price === 0 || props.no_market === true) {
-    return 0;
-  }
-  if (props.price_vwap > props.is_price) {
-    return -(1 - props.is_price / props.price_vwap) * 100;
-  }
-  if (props.price_vwap <= props.is_price) {
-    return (props.is_price / props.price_vwap - 1) * 100;
-  }
-  return 0;
-}
 </script>
 <style scoped></style>
