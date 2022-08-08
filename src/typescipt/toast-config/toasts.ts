cc5c3dd2b25b60e796082a4f3ecd258e318ce2e3
skullandbonesdao <skullandbonesdao@gmail.com>
2022-08-08 12:20:24 +0200

    increased toast time to 5s

--- src/typescipt/toast-config/toasts.ts
+++ src/typescipt/toast-config/toasts.ts
@@ -1,19 +1,19 @@
 import { ToastOptions } from "mosha-vue-toastify";
 
 export const TOAST_INFO: ToastOptions = {
-  timeout: 3000,
+  timeout: 5000,
   position: "bottom-right",
   type: "info",
 };
 
 export const TOAST_SUCCESS: ToastOptions = {
-  timeout: 3000,
+  timeout: 5000,
   position: "bottom-right",
   type: "success",
 };
 
 export const TOAST_ERROR: ToastOptions = {
-  timeout: 3000,
+  timeout: 5000,
   position: "bottom-right",
   type: "danger",
 };
